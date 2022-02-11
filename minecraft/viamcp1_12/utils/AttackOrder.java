2ef1cbb5e0d873242c4ca236a8e1445204392972
Foreheadchann <foreheadchann@googlemail.com>
2022-02-11 22:12:22 +0200

    Added AttackOrder for 1.12.2

---
+++ minecraft/viamcp1_12/utils/AttackOrder.java
@@ -0,0 +1,50 @@
+package viamcp.utils;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.RayTraceResult;
+import viamcp.ViaMCP;
+import viamcp.protocols.ProtocolCollection;
+
+public class AttackOrder
+{
+    private static final Minecraft mc = Minecraft.getMinecraft();
+
+    private static final int VER_1_8_ID = 47;
+
+    public static void sendConditionalSwing(RayTraceResult mop, EnumHand enumHand)
+    {
+        if (mop != null && mop.typeOfHit != RayTraceResult.Type.ENTITY)
+        {
+            mc.player.swingArm(enumHand);
+        }
+    }
+
+    public static void sendFixedAttack(EntityPlayer entityIn, Entity target, EnumHand enumHand)
+    {
+        // Using this instead of ViaMCP.PROTOCOL_VERSION so does not need to be changed between 1.8.x and 1.12.2 base
+        // getVersion() can be null, but not in this case, as ID 47 exists, if not removed
+        if(ViaMCP.getInstance().getVersion() == ProtocolCollection.getProtocolById(VER_1_8_ID).getVersion())
+        {
+            send1_8Attack(entityIn, target, enumHand);
+        }
+        else
+        {
+            send1_9Attack(entityIn, target, enumHand);
+        }
+    }
+
+    private static void send1_8Attack(EntityPlayer entityIn, Entity target, EnumHand enumHand)
+    {
+        mc.player.swingArm(enumHand);
+        mc.playerController.attackEntity(entityIn, target);
+    }
+
+    private static void send1_9Attack(EntityPlayer entityIn, Entity target, EnumHand enumHand)
+    {
+        mc.playerController.attackEntity(entityIn, target);
+        mc.player.swingArm(enumHand);
+    }
+}
